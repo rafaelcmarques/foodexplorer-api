c0df26e40791fee084c34276389fe47f1c94ee93
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-06-17 20:06:15 -0300

    adicionado pm2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 
 const sqliteConnection = require("./database/sqlite");
 const AppError = require("./utils/AppError");
@@ -33,5 +34,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
